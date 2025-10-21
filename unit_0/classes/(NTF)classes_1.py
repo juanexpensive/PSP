acd37c5db96a9d1c202a95463970c24c04ca86d3
juanexpensive <carovaquerojuan@gmail.com>
2025-10-21 14:39:12 +0200

    Update (NTF)classes_1.py

--- unit_0/classes/(NTF)classes_1.py
+++ unit_0/classes/(NTF)classes_1.py
@@ -1,6 +1,26 @@
+<<<<<<< Updated upstream
 class CheckingAccount:
     def __init__(self, DNI, initial_balance, name=""):
         #Initialize a checking account. If the name is not provided, it defaults to an empty string.
+=======
+#NEED TO FINISH
+"""
+Diseñar la clase CuentaCorriente, que almacena los datos DNI, nombre y el saldo. 
+Añade los siguientes constructores:
+Con el DNI del titular de la cuenta y un saldo inicial. El nombre se inicializará a cadena vacía.
+Con el DNI, nombre y el saldo inicial.
+Las operaciones típicas de una cuenta corriente son:
+Sacar dinero: el método debe indicar si ha sido posible llevar a cabo la operación, 
+si existe saldo suficiente. Si es posible llevar a cabo la operación se resta la cantidad a sacar al saldo de la cuenta.
+Ingresar dinero: se incrementa el saldo.
+Crear también los métodos __str__, __eq__ y __lt__. 
+Se considera que dos cuentas corrientes son iguales si tienen el mismo DNI. 
+Las cuentas corrientes se ordenarán de menor a mayor por el saldo.
+"""
+
+class CuentaCorriente:
+    def __init__(self, DNI, name, balance): 
+>>>>>>> Stashed changes
         self.DNI = DNI
         self.name = name
         self.balance = initial_balance
